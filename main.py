c4f299f7ff9bea0023fd1d1250f2dde0c0c2b6a2
Hassan Kashif <rhkashif@gmail.com>
2023-11-23 15:35:04 +0000

    Added module docstring

---
+++ main.py
@@ -0,0 +1,18 @@
+"""
+Scrabble Game Main Module
+
+This module is the entry point for a Scrabble-like word game implemented in Python. 
+
+The game logic includes word validation and score calculation
+
+Usage:
+    Run this module to start the game. Players can interact with the game through 
+    a command-line interface.
+
+Note:
+    This game is designed for educational purposes and as a demonstration of Python programming skills. 
+    It is not intended for commercial use.
+
+Author: Hassan Kashif
+"""
+
