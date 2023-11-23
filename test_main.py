2beb3252f6d9b119028a91f2ed471c029da0988c
Hassan Kashif <rhkashif@gmail.com>
2023-11-23 15:42:48 +0000

    added testing file and module docstring

---
+++ test_main.py
@@ -0,0 +1,17 @@
+"""
+Scrabble Game Test Module
+
+This test module contains a suite of unit tests for the Scrabble game implemented in Python. 
+It is designed to test the functionality of various components and functions defined in the 
+main game module. These tests ensure the correctness of game logic, including word validation, 
+score calculation and tile distribution.
+
+The tests are organized by functionality, with each test case focusing on a specific aspect 
+of the game. This organization helps in identifying and isolating bugs or issues in the codebase.
+
+Note:
+    It's important to keep the tests up-to-date with any changes in the game's logic or 
+    functionality to maintain the reliability of the tests.
+
+Author: Hassan Kashif
+"""
